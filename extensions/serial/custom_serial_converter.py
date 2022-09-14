c727d8538385fbac95f53a07e84ed3c4ff2084d2
JoeDang <josephdang999@gmail.com>
2022-09-14 14:10:18 +0700

    Add timestamp of gateway to message

--- extensions/serial/custom_serial_converter.py
+++ extensions/serial/custom_serial_converter.py
@@ -13,7 +13,7 @@
 #     limitations under the License.
 
 from thingsboard_gateway.connectors.converter import Converter, log
-
+from datetime import datetime
 
 class CustomSerialUplinkConverter(Converter):
     def __init__(self, config):
@@ -52,5 +52,7 @@ class CustomSerialUplinkConverter(Converter):
                     #converted_data = {config_object['key']: data_to_convert.decode('UTF-8')}
                     converted_data = {config_object['key']: data_to_convert}
                     self.result_dict[key].append(converted_data)
+                timestamp_data = {'ts': str(int(datetime.now().timestamp()))}
+                self.result_dict[key].append(timestamp_data)
         log.debug("Converted data: %s", self.result_dict)
         return self.result_dict
